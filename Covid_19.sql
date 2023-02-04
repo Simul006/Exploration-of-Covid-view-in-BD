e863be25e47dbbd34af8470cce08549e41af9083
Simul Reza <48527475+Simul006@users.noreply.github.com>
2023-02-04 12:41:16 +0100

    Update and rename 1st.sql to Covid_19.sql

--- Covid_19.sql
+++ Covid_19.sql
@@ -1,10 +1,15 @@
 use covid_19;
+
 -- show the covid deaths table 
+
 select *
 from covid_deaths 
 where continent is not null
 order by 1,2;
+
+
 -- show the covid vaccinations table
+
 select *
 from covid_vaccinations
 where continent is not null limit 50;
@@ -19,36 +24,54 @@ select location,date,total_cases,total_deaths, (total_deaths/total_cases)*100 AS
 from covid_deaths 
 where location ='Bangladesh'
 order by 1,2;
+
+
 -- Looking the number of total cases vs population
+
 select location,date,population,total_cases, (total_cases/population)*100 AS PercentagePopulationInfected
 from covid_deaths 
 where location ='Bangladesh'
 order by 1,2;
+
+
 -- Looking at countries with highest infection rate compare to population
+
 select location,population,max(total_cases) as HighestInfectionCount, max((total_cases/population))*100 AS PercentagePopulationInfected
 from covid_deaths 
 -- where location ='Bangladesh'
 group by location,population 
 order by PercentagePopulationInfected DESC;
+
+
 -- Looking at countries with highest death rate 
+
 select location, max(total_deaths) as TotalDeathCount
 from covid_deaths
 where continent is not null
 group by location
 order by TotalDeathCount desc;
+
+
 -- Looking at continent with highest death rate 
+
 select continent, max(total_deaths) as TotalDeathCount
 from covid_deaths
 where continent is not null
 group by continent
 order by TotalDeathCount desc;
+
+
 -- Looking global death percentage 
+
 select sum(new_cases) as total_cases,sum(new_deaths) as total_deaths,sum(new_deaths)/sum(new_cases) as DeathPercentage
 from covid_deaths
 where continent is not null
 -- group by date
 order by 1,2;
+
+
 -- Looking at total population vs vaccination accross the world 
+
 select de.continent,de.location,de.date,de.population,va.new_vaccinations,
 sum(va.new_vaccinations) over (partition by de.location) as RollingPeopleVaccinated
 from covid_deaths de
@@ -58,7 +81,10 @@ and de.date = va.date
 where de.continent is not null
 order by 2,3
 ;
--- Looking the percentage of people got vaccinated in bangladesh. 
+
+
+-- Looking the percentage of people got vaccinated in Bangladesh 
+
 with PopVsVac ( continent,location,date,population,new_vaccinations,RollingPeopleVaccinated)
 as 
 (
@@ -76,11 +102,8 @@ from PopVsVac
 where location like '%bang%';
 
 
-
-
-
 -- Temp Table 
-
+DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
 (
 Continent nvarchar(255),
@@ -90,7 +113,6 @@ Population numeric,
 New_vaccinations numeric,
 RollingPeopleVaccinated numeric
 )
-
 insert into #percentPopulationVaccinated
 select de.continent,de.location,de.date,de.population,va.new_vaccinations,
 sum(va.new_vaccinations) over (partition by de.location) as RollingPeopleVaccinated
@@ -102,8 +124,6 @@ where de.continent is not null
 order by 2,3
 select *, (RollingPeopleVaccinated/population)*100 
 from #percentPopulationVaccinated;
-
-
 
 
 -- Creating View to store data for later visualizations
